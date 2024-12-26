2ad9d11a38e56340de6abd7d3a9776766d630ba2
Ahyun-Song <yoon9113@naver.com>
2024-12-26 10:24:55 +0900

    람다식, 구조체 사용 수정

--- WindowsFormsApp_241226/Form1.cs
+++ WindowsFormsApp_241226/Form1.cs
@@ -8,23 +8,91 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace calculating_machine
 {
+    public struct Calculator
+    {
+        public string ResultValue { get; private set; }
+        public string CurrentValue { get; private set; }
+        public string Operation { get; private set; }
+
+        public Calculator(string resultValue, string currentValue, string operation)
+        {
+            ResultValue = resultValue;
+            CurrentValue = currentValue;
+            Operation = operation;
+        }
+
+        public void SetCurrentValue(string value) => CurrentValue = value;
+
+        public void SetOperation(string operation)
+        {
+            if (!string.IsNullOrEmpty(CurrentValue))
+            {
+                ResultValue = string.IsNullOrEmpty(ResultValue) ? CurrentValue : ResultValue;
+                CurrentValue = "";
+                Operation = operation;
+            }
+        }
+
+        public void ClearAll()
+        {
+            ResultValue = "";
+            CurrentValue = "";
+            Operation = "";
+        }
+
+        public void ClearCurrent() => CurrentValue = "";
+
+        public string Calculate()
+        {
+            double result;
+            double.TryParse(ResultValue, out double left);
+            double.TryParse(CurrentValue, out double right);
+
+            switch (Operation)
+            {
+                case "+":
+                    result = left + right;
+                    break;
+                case "-":
+                    result = left - right;
+                    break;
+                case "*":
+                    result = left * right;
+                    break;
+                case "/":
+                    if (right == 0) return "0으로 나눌 수 없습니다.";
+                    result = left / right;
+                    break;
+                default:
+                    return "오류";
+            }
+
+            ResultValue = result.ToString();
+            CurrentValue = "";
+            Operation = "";
+            return ResultValue;
+        }
+    }
+
     public partial class Form1 : Form
     {
-        calculate cal = new calculate();
-        string before_Value = "";
-        string current_Value = "";
-        string result_Value = "";
-        string operation = "";
+        private Calculator calculator;
 
-        bool isEqualPressed = false;
         public Form1()
         {
             InitializeComponent();
+            calculator = new Calculator("", "", "");
         }
 
-        // 숫자 버튼 핸들러 (화살표 함수)
+        private void AddNumber(string number)
+        {
+            calculator.SetCurrentValue(calculator.CurrentValue + number);
+            textBox_presentvalue.Text = calculator.CurrentValue;
+        }
+
         private void button_number1_Click(object sender, EventArgs e) => AddNumber("1");
         private void button_number2_Click(object sender, EventArgs e) => AddNumber("2");
         private void button_number3_Click(object sender, EventArgs e) => AddNumber("3");
@@ -34,191 +102,47 @@ namespace calculating_machine
         private void button_number7_Click(object sender, EventArgs e) => AddNumber("7");
         private void button_number8_Click(object sender, EventArgs e) => AddNumber("8");
         private void button_number9_Click(object sender, EventArgs e) => AddNumber("9");
-        private void button_number0_Click(object sender, EventArgs e)
-            => current_Value = current_Value != "0" ? current_Value + "0" : current_Value;
-
-        private void button_ce_Click(object sender, EventArgs e) => ClearCurrentValue();
-        private void button_c_Click(object sender, EventArgs e) => ClearAllValues();
-        private void button_backspace_Click(object sender, EventArgs e) => RemoveLastCharacter();
+        private void button_number0_Click(object sender, EventArgs e) => AddNumber("0");
 
         private void button_add_Click(object sender, EventArgs e) => SetOperation("+");
         private void button_subtract_Click(object sender, EventArgs e) => SetOperation("-");
         private void button_multiple_Click(object sender, EventArgs e) => SetOperation("*");
         private void button_divide_Click(object sender, EventArgs e) => SetOperation("/");
 
-        private void button_covertsign_Click(object sender, EventArgs e) => ConvertSign();
-        private void button_percent_Click(object sender, EventArgs e) => ApplyPercentage();
-        private void button_invert_Click(object sender, EventArgs e) => ApplyInverse();
-        private void button_spuare_Click(object sender, EventArgs e) => ApplySquare();
-        private void button_squareroot_Click(object sender, EventArgs e) => ApplySquareRoot();
-        private void button_float_Click(object sender, EventArgs e)
-            => current_Value = current_Value.IndexOf(".") == -1 ? current_Value + "." : current_Value;
-
-        private void button_equal_Click(object sender, EventArgs e) => CalculateResult();
-       
-        private void AddNumber(string number)
+        private void SetOperation(string op)
         {
-            current_Value += number;
-            textBox_presentvalue.Text = current_Value;
-        }
-
-        private void ClearCurrentValue()
-        {
-            current_Value = "";
+            calculator.SetOperation(op);
+            textBox_prevalue.Text = calculator.ResultValue + op;
             textBox_presentvalue.Text = "";
-            operation = "";
         }
 
-        private void ClearAllValues()
+        private void button_equal_Click(object sender, EventArgs e)
         {
-            current_Value = "";
-            result_Value = "";
-            before_Value = "";
-            operation = "";
+            string result = calculator.Calculate();
+            textBox_presentvalue.Text = result;
+            textBox_prevalue.Text = "";
+        }
+
+        private void button_c_Click(object sender, EventArgs e)
+        {
+            calculator.ClearAll();
             textBox_presentvalue.Text = "";
             textBox_prevalue.Text = "";
         }
 
-        private void RemoveLastCharacter()
+        private void button_ce_Click(object sender, EventArgs e)
         {
-            if (current_Value.Length > 0)
-            {
-                current_Value = current_Value.Substring(0, current_Value.Length - 1);
-                textBox_presentvalue.Text = current_Value;
-            }
+            calculator.ClearCurrent();
+            textBox_presentvalue.Text = "";
         }
 
-        private void SetOperation(string op)
+        private void button_backspace_Click(object sender, EventArgs e)
         {
-            if (current_Value.Length != 0)
+            if (!string.IsNullOrEmpty(calculator.CurrentValue))
             {
-                if (result_Value == "")
-                {
-                    result_Value = textBox_presentvalue.Text;
-                }
-                textBox_prevalue.Text = result_Value + op;
-                current_Value = "";
-                textBox_presentvalue.Text = "";
-                operation = op;
+                calculator.SetCurrentValue(calculator.CurrentValue.Substring(0, calculator.CurrentValue.Length - 1));
+                textBox_presentvalue.Text = calculator.CurrentValue;
             }
-        }
-
-        private void ConvertSign()
-        {
-            if (textBox_presentvalue.Text.Length != 0)
-            {
-                current_Value = textBox_presentvalue.Text;
-                double resConvert = double.Parse(current_Value) * -1;
-                current_Value = resConvert.ToString();
-                textBox_presentvalue.Text = current_Value;
-            }
-        }
-
-        private void ApplyPercentage()
-        {
-            if (current_Value.Length != 0)
-            {
-                if (result_Value == "")
-                {
-                    result_Value = "0";
-                    current_Value = "";
-                    textBox_prevalue.Text = result_Value;
-                    textBox_presentvalue.Text = "";
-                }
-                else if (result_Value != "")
-                {
-                    result_Value = (double.Parse(result_Value) / 100).ToString();
-                    current_Value = "";
-                    textBox_prevalue.Text = result_Value;
-                    textBox_presentvalue.Text = "";
-                }
-            }
-        }
-
-        private void ApplyInverse()
-        {
-            if (textBox_presentvalue.Text.Length != 0)
-            {
-                result_Value = cal.GetInverse_Value(textBox_presentvalue.Text);
-                current_Value = "";
-                textBox_prevalue.Text = "1/(" + textBox_presentvalue.Text + ")";
-                textBox_presentvalue.Text = result_Value;
-            }
-            else if (textBox_presentvalue.Text.Length == 0)
-            {
-                textBox_presentvalue.Text = "0으로 나누지 마세요!";
-                textBox_prevalue.Text = "1/0";
-            }
-        }
-
-        private void ApplySquare()
-        {
-            if (textBox_presentvalue.Text.Length != 0)
-            {
-                result_Value = cal.GetSquare_Value(textBox_presentvalue.Text);
-                textBox_prevalue.Text = "sqrt(" + textBox_presentvalue.Text + ")";
-                current_Value = "";
-                textBox_presentvalue.Text = result_Value;
-            }
-        }
-
-        private void ApplySquareRoot()
-        {
-            if (textBox_presentvalue.Text.Length != 0)
-            {
-                result_Value = cal.GetRoot_Value(textBox_presentvalue.Text);
-                if (result_Value == "입력이 잘못되었습니다.")
-                {
-                    textBox_presentvalue.Text = result_Value;
-                    result_Value = "";
-                    textBox_prevalue.Text = "";
-                }
-                else
-                {
-                    current_Value = "";
-                    textBox_prevalue.Text = "sqrt(" + textBox_presentvalue.Text + ")";
-                    textBox_presentvalue.Text = result_Value;
-                }
-            }
-        }
-
-        private void CalculateResult()
-        {
-            switch (operation)
-            {
-                case "+":
-                    if (current_Value == "") break;
-                    result_Value = cal.addtion(result_Value, current_Value);
-                    textBox_prevalue.Text += current_Value;
-                    textBox_presentvalue.Text = result_Value;
-                    break;
-                case "-":
-                    if (current_Value == "") break;
-                    result_Value = cal.subtraction(result_Value, current_Value);
-                    textBox_prevalue.Text += current_Value;
-                    textBox_presentvalue.Text = result_Value;
-                    break;
-                case "*":
-                    if (current_Value == "") break;
-                    result_Value = cal.multiplication(result_Value, current_Value);
-                    textBox_prevalue.Text += current_Value;
-                    textBox_presentvalue.Text = result_Value;
-                    break;
-                case "/":
-                    if (current_Value == "") break;
-                    result_Value = cal.division(result_Value, current_Value);
-                    if (result_Value == "∞")
-                    {
-                        textBox_presentvalue.Text = "0으로 나눌 수 없습니다.";
-                        operation = "";
-                        result_Value = "";
-                        textBox_prevalue.Text = "";
-                        break;
-                    }
-                    textBox_presentvalue.Text = result_Value;
-                    break;
-            }
-            operation = "";
         }
     }
 }
